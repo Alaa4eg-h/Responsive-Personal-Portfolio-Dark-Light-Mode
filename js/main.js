42d6390d4640c098157dbfd2d21726d08cd439f3
Alaa4eg-h <Alaa4eg@outlook.com>
2021-05-25 01:19:16 +0200

    Contact Section

--- js/main.js
+++ js/main.js
@@ -104,7 +104,21 @@ let swiper = new Swiper(".portfolio__container", {
   },
 });
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidePreView: 2,
+    },
+  },
+});
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
